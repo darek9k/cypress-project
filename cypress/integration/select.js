1709dfffa162be29242173eabf5500a4d1462f6a
darek <darecki9k@gmail.com>
2024-01-29 15:55:21 +0100

    select

---
+++ cypress/integration/select.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe("E2E - Select option", () => {
+    it("Select option", () => {
+        cy.visit("https://magento.softwaretestingboard.com/gear/bags.html");
+
+        //byName
+        cy.get("#sorter").select("Option 1")
+
+        //byValue
+        cy.get("#sorter").select("2")
+
+        //byIndex
+        cy.get('#sorter').select(1)
+        
+    })
+
+    it.only("All Options", () => {
+        cy.visit("https://magento.softwaretestingboard.com/gear/bags.html");
+
+        cy.get("#sorter").then(select => {
+            cy.wrap(select).find("option").each(option => {
+                cy.wrap(option).invoke('text').then(text => {
+                    cy.log(text);
+                })
+            })
+        })
+    })
+})
