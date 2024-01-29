27d278009756529ff5c2539bc36b36123ff23bd8
darek <darecki9k@gmail.com>
2024-01-29 12:32:40 +0100

    invoke

--- cypress/integration/invoke.js
+++ cypress/integration/invoke.js
@@ -3,8 +3,24 @@
 describe("E2E - Invoke Metod", () => {
     it("Invoke", () => {
         cy.visit("/");
+
+        // /Retrieving the value of a given elementn
         cy.get("#ui-id-8").invoke("text").then(text => {
             cy.log(text)
-        });
+        })
+
+        //Accessing the value of an attribute
+        cy.get("#ui-id-8").invoke("attr", "href").then(link => {
+            cy.log(link)
+        })
+
+        cy.get("#ui-id-8").invoke("attr", "role").then(role => {
+            cy.log(role)
+        })
+
+        //Getting values
+        cy.get("#search").type("Sample value").invoke("prop", "value").then(value => {
+            cy.log(value)
+        })
+        })
     })
-})
